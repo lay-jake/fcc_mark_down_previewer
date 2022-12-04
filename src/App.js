8b0762e44fe746b10d30442a96f2cd41d4ad6ca4
Jacob D Lay <erwinscatlives@gmail.com>
2022-12-03 22:51:20 -0600

    Ready for Submit

---
+++ src/App.js
@@ -0,0 +1,73 @@
+
+import Markdown from 'marked-react';
+
+import { useState } from 'react';
+import './App.css';
+
+const defaultText = `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.org), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)`
+
+function App() {
+  const [markDownStuff,setMarkDownStuff] = useState({value:defaultText});
+
+  function handleChange(event){
+    setMarkDownStuff({value:event.target.value})
+  }
+
+  console.log(markDownStuff)
+  return (
+    <div className="App">
+      <header className="App-header">
+       <label>Type Markdown Here:</label>
+        <textarea id='editor' value={markDownStuff.value} onChange ={handleChange}></textarea>
+        <div id='preview'>   
+        <Markdown>{markDownStuff.value}</Markdown>
+        </div>
+   
+      </header>
+    </div>
+  );
+}
+
+export default App;
